cf9fe595ce69c65536cee514a320a33ae831c8dc
Dmitriy Kravchenko <drkravchenko@vtb.ru>
2024-11-12 23:08:48 +0300

    refactor: update commitlint config

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -1,7 +1,7 @@
 let pullRequestRegexp = /^Merge pull request #\d+:.*/s;
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    ignores: [(commit) => pullRequestRegexp.test(commit)],
+    ignores: [(commit) => pullRequestRegexp.test(commit), (commit) => commit.startsWith('i,rv'), (commit) => commit.startsWith('active')],
     rules: {
         "body-max-line-length": [1, "always", 100],
     },
