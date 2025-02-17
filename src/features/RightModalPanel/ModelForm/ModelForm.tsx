a526d8f4c49908d80e39695d5e0fead1f1bc10dc
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-02-17 18:12:58 +0300

    fix: add can edit artefact

--- src/features/RightModalPanel/ModelForm/ModelForm.tsx
+++ src/features/RightModalPanel/ModelForm/ModelForm.tsx
@@ -418,17 +418,17 @@ export const ModelForm = ({
   }, [initialRow, artifacts]);
 
   useEffect(() => {
-    // if (!isEditByRatingModel) {
-    //   setActiveModelByDefault(false);
-    //   return;
-    // }
+    if (!isEditByRatingModel) {
+      setActiveModelByDefault(false);
+      return;
+    }
 
     if (activeRow?.active_model === '1') {
       setActiveModelByDefault(true);
     }
   }, [
     activeRow?.active_model, 
-    // isEditByRatingModel
+    isEditByRatingModel
   ]);
 
   // Scroll to edit input field
@@ -498,7 +498,7 @@ export const ModelForm = ({
               dimension="s"
               checked={activeModelByDefault}
               onChange={activeModelCheckboxHandler}
-              // disabled={!isEditByRatingModel}
+              disabled={!isEditByRatingModel}
             >
               Действующая Модель/Модуль
             </CheckboxField>
--- src/features/RightModalPanel/helpers.ts
+++ src/features/RightModalPanel/helpers.ts
@@ -492,9 +492,9 @@ const isFieldDisabled = (
   const isControlledByConditions =
     fieldSchema?.enabledByValueConditions && isDisabledByValueConditions && isDisabledByConditions;
 
-  // const isDisabledArtifactBySource = !canEditArtefact(artifact, row);
+  const isDisabledArtifactBySource = !canEditArtefact(artifact, row);
 
-  return isGloballyDisabled || isControlledByConditions;
+  return isGloballyDisabled || isControlledByConditions || isDisabledArtifactBySource;
 };
 
 // Main mapping function that combine object for proper input format
