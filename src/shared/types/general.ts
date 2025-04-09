4a8b967d68053274da30139bab28545266980ec4
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-04-09 12:06:52 +0300

    feat: new attributes

--- src/shared/types/general.ts
+++ src/shared/types/general.ts
@@ -180,6 +180,9 @@ type Row = ArtificialRow & {
   operational_monitoring: string | null;
   analytical_monitoring: string | null;
   bpmn_key: string | null;
+  output_table: string | null;
+  allocation_assessment_class: string | null;
+  allocation_assessment_parameters: string | null;
 };
 
 type ColumnsFilter = Record<keyof Row, Array<string>>;
