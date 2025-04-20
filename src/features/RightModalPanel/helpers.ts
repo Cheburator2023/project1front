8899d28a78e81bee11aa5793d6df6d69f7f010b3
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-04-20 15:57:43 +0300

    fix: get query parametrs

--- src/features/RightModalPanel/helpers.ts
+++ src/features/RightModalPanel/helpers.ts
@@ -29,8 +29,6 @@ import {
 import { ArtifactGroup } from '@src/shared/api/types';
 import { concat, uniqBy } from 'lodash';
 import { CUSTOMER_MAP, CUSTOMER_TYPE } from '@src/shared/constants/customers';
-import { useUserStore } from '@src/shared/stores';
-import { isInBusinessCustomers, isModelCreator } from '@src/shared/helpers';
 import {
   FormFieldConditions,
   FormFieldValueConditions,
@@ -429,24 +427,21 @@ const getDisabledStatus = (minDate: Date, maxDate: Date, quarter: number, canEdi
   return !isWithinInterval(currentDate, { start: minDate, end: maxDate });
 };
 
-const canEditArtefact = (artifact?: Artifact, row?: Partial<Row> | undefined): boolean => {
-  if (!artifact) {
-    return false;
-  }
+const canEditArtefact = (artifact?: Artifact, row?: Partial<Row>): boolean => {
+  if (!artifact) return false;
+  if (!row) return true;
 
-  if (!row) {
-    return true;
-  }
+  const isEditableBySum = artifact.is_editable_by_role_sum === '1';
+  const isEditableBySumRm = artifact.is_editable_by_role_sum_rm === '1';
 
-  if (row.model_source === ModelSource.SUM_RM) {
-    return artifact.is_editable_by_role_sum_rm === '1';
+  switch (row.model_source) {
+    case ModelSource.SUM:
+      return isEditableBySum;
+    case ModelSource.SUM_RM:
+      return isEditableBySumRm;
+    default:
+      return false;
   }
-
-  if (row.model_source === ModelSource.SUM) {
-    return artifact.is_editable_by_role_sum === '1';
-  }
-
-  return false;
 };
 
 const isFieldDisabled = (
