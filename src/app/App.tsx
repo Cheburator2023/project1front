a2f2c59b6cf2f69c39c76fc678ad05a0c4d5e166
synikolaev <synikolaev@inno.tech>
2025-09-03 15:29:12 +0300

    feat: new template types as in ag-grid model

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import RoutesComponent from './Routes';
 import Layout from './Layout';
 
+const IS_DEV = process.env.NODE_ENV === 'development';
+
 export const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename={IS_DEV ? '/sum-rm' : '/'}>
         <Layout>
           <RoutesComponent />
         </Layout>
