6d7cb4cf94153065dcc68086f96f62b6ebfcc03a
synikolaev <synikolaev@inno.tech>
2026-04-22 13:41:11 +0300

    refactor: improve changelog version parsing logic to handle multiple header formats

--- webpack.version.js
+++ webpack.version.js
@@ -4,17 +4,23 @@ const path = require('path');
 /**
  * Парсит первую (самую свежую) запись CHANGELOG.md вида:
  *   # [1.60.0](<url>) (2026-04-20)
+ *   ## [1.60.1](<url>) (2026-04-22)
  * и возвращает { version, date }. При неудаче возвращает { version: '', date: '' }.
  */
 function readChangelogVersion() {
   try {
     const changelogPath = path.resolve(__dirname, 'CHANGELOG.md');
     const content = fs.readFileSync(changelogPath, 'utf8');
-    const match = content.match(
-      /^#\s*\[(\d+\.\d+\.\d+)\][^\n]*\((\d{4}-\d{2}-\d{2})\)/m,
-    );
-    if (match) {
-      return { version: match[1], date: match[2] };
+
+    for (const line of content.split(/\r?\n/)) {
+      const versionMatch = line.match(/^#{1,6}\s*\[(\d+\.\d+\.\d+)\]/);
+      if (!versionMatch) continue;
+
+      const dateMatch = line.match(/\((\d{4}-\d{2}-\d{2})\)/);
+      return {
+        version: versionMatch[1],
+        date: dateMatch ? dateMatch[1] : '',
+      };
     }
   } catch (_) {
     // noop
